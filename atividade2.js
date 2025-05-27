3b61a431962f2b2f099106d6ac1c9058b3fce9c8
Ludmily Oliveira <ludmily.oliveira@hotmail.com>
2025-05-27 16:41:28 -0300

    Atualização atividade 2

--- atividade2.js
+++ atividade2.js
@@ -6,14 +6,15 @@ nome = leia.question("Digite o nome do doador: ")
 
 idade = leia.questionInt("Digite a idade do doador: ")
 
-doacao = leia.keyInYNStrict("Primeira doacao: ", "Responda apenas com Y ou N")
+console.log('\nPrimeira doação = true \nJá doou anteriormente = false')
+doacao = leia.question('\nPrimeira Doacao? ', 'Responda com true ou false\n')
 
 if(idade >= 18 && idade <60){
     console.log(nome, "está apto para doar sangue")
-} else if (idade >=60 && idade <= 69 && doacao === "Y"){
+} else if (idade >=60 && idade <= 69 && doacao === "false"){
     console.log(nome, "está apto para doar sangue")
-} else if (idade >=60 && idade <= 69 && doacao === "N"){
+} else if (idade >=60 && idade <= 69 && doacao === "true"){
     console.log(nome, "não está apto para doar sangue")
 } else {
-    console.log(nome, "não está apto para doar sangue")
+    console.log(nome, "não está apto para doar sangue!")
 }
